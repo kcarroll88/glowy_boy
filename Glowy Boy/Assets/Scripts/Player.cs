6a38e05ce04118c85aa316a4dd1937a347bde28e
Keenan <keenan.carroll.va@gmail.com>
2019-04-08 14:35:10 -0400

    Climbing

--- Glowy Boy/Assets/Scripts/Player.cs
+++ Glowy Boy/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -30,6 +31,7 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        Climbing();
         FlipSprite();
     }
 
@@ -41,6 +43,18 @@ public class Player : MonoBehaviour
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Climbing()
+    {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))) { return; }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        bool playerHasVericalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVericalSpeed);
     }
 
     private void Jump()
