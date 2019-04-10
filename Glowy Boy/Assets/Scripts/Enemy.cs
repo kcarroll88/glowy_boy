e600472389f8e520162be85ffa8ee572602f785f
Keenan <keenan.carroll.va@gmail.com>
2019-04-10 13:08:35 -0400

    Enemy Movement and Flipping

--- Glowy Boy/Assets/Scripts/Enemy.cs
+++ Glowy Boy/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +7,34 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float hopSpeed = 5f;
 
+    Rigidbody2D myRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Hop();  
+        if (IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(hopSpeed, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-hopSpeed, 0f);
+        }
     }
 
-    private void Hop()
+    bool IsFacingRight()
     {
+        return transform.localScale.x > 0;
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
     }
 }
