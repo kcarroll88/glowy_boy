4bab9f4a87dcd5c3db794b0a4051f04d2ac5a9d4
Keenan <keenan.carroll.va@gmail.com>
2019-04-10 10:38:50 -0400

    Added Enemies

---
+++ Glowy Boy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float hopSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Hop();  
+    }
+
+    private void Hop()
+    {
+
+    }
+}
