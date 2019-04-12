3a2a2ddc4b2a11946012c48d61c842bb6c9b757b
Keenan <keenan.carroll.va@gmail.com>
2019-04-12 15:32:55 -0400

    Built End of Level. Can now progress through levels

---
+++ Glowy Boy/Assets/LevelLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
