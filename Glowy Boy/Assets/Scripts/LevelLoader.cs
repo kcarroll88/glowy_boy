9a49388f8d459084d8bc73a827bd70518c071520
Keenan <keenan.carroll.va@gmail.com>
2019-04-15 11:01:00 -0400

    minor change

--- Glowy Boy/Assets/Scripts/LevelLoader.cs
+++ Glowy Boy/Assets/Scripts/LevelLoader.cs
@@ -7,7 +7,7 @@ public class LevelLoader : MonoBehaviour
 {
     public void LoadNextScene()
     {
-        var sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(sceneIndex + 1);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
