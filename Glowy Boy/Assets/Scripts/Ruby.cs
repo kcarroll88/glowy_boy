702090a23d62dff403e572c10079593f2845ef93
Keenan <keenan.carroll.va@gmail.com>
2019-04-15 14:44:40 -0400

    Sounds for the ruby pick ups

--- Glowy Boy/Assets/Scripts/Ruby.cs
+++ Glowy Boy/Assets/Scripts/Ruby.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Ruby : MonoBehaviour
 {
+    [SerializeField] AudioClip rubyPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(rubyPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
