8e6fc0547ebe9fdcc28ac1f5071cad9967bb9cbe
Keenan <keenan.carroll.va@gmail.com>
2019-04-16 12:40:25 -0400

    building cloud spawner

---
+++ Glowy Boy/Assets/CloudSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> clouds;
+    [SerializeField] float cloudSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnCloud()
+    {
+        return;
+    }
+}
